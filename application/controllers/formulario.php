f658243f62223e56c871b2d30dfffb366e7df01c
Jonathan Villamizar <jvilla8a@gmail.com>
2015-10-29 09:28:10 -0500

    adding html, styles, js and others

---
+++ application/controllers/formulario.php
@@ -0,0 +1,21 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * Description of formulario
+ *
+ * @author 5-09
+ */
+
+class formulario extends CI_Controller {
+
+	public function index(){
+			$this->load->view('main');
+	}
+
+}
+
+?>
