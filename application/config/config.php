0ab9b0d1645ff1edbecc858f567d5bfa0ac9ca24
jvilla8a <jvilla8a@gmail.com>
2015-12-04 17:27:32 -0500

    reverting lastest comit

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'https://pbets.herokuapp.com//bets/';
+$config['base_url'] = 'http://localhost:8080/bets/';
 
 /*
 |--------------------------------------------------------------------------
